fa50d03b8db4d879b2ffbcbace7193a2d72af08f
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-18 02:31:50 +0900

    ✨ feat: service 생성

---
+++ src/main/java/com/example/mokkoji/domain/tag/service/TagService.java
@@ -0,0 +1,9 @@
+package com.example.mokkoji.domain.tag.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class TagService {
+}
