ebb3052184623b7284d56dbb4fd1a545c06eb6d2
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-18 20:36:32 +0900

    ✨ feat: 유저 dto, repo 생성

--- src/main/java/com/example/mokkoji/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/mokkoji/domain/user/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.example.mokkoji.domain.user.repository;
 
+import com.example.mokkoji.domain.user.entity.OAuthProvider;
 import com.example.mokkoji.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findBySocialAndSocialId(OAuthProvider social, String socialId);
 
 }
