ef900496c045eccc5c44e999267101b7259348dd
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-20 02:09:34 +0900

    ✨ feat: Store repo 구현

--- src/main/java/com/example/mokkoji/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/mokkoji/domain/store/repository/StoreRepository.java
@@ -2,9 +2,20 @@ package com.example.mokkoji.domain.store.repository;
 
 import com.example.mokkoji.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
+//    List<Store> findAll(double lat, double lng);
+
+    @Query("SELECT s FROM Store s WHERE s.location.addressDong LIKE %:동%")
+    List<Store> findByAddressDong(@Param("동") String 동);
+
+    @Query("SELECT s FROM Store s WHERE s.name LIKE %:keyword%")
+    List<Store> findByStoreName(String keyword);
 }
