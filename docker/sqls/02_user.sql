fbf0d758a63bff825bd8a42b2afe285502cb7c4e
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-23 12:25:50 +0900

    🔥 fix: 데이터 삽입

---
+++ docker/sqls/02_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO `user` (username, email, password, role, provider, social_id)
+VALUES
+    ('홍길동', 'hong@example.com', 'password123', 'PERSONAL', 'EMAIL', NULL),
+    ('김영희', 'younghee@kakao.com', 'kakao_pw', 'PERSONAL', 'KAKAO', 'kakao_112233'),
+    ('박상민', 'sangmin@example.com', 'pass456', 'BUSINESS', 'EMAIL', NULL),
+    ('이수진', 'soo@kakao.com', 'kakaopass', 'BUSINESS', 'KAKAO', 'kakao_998877');
