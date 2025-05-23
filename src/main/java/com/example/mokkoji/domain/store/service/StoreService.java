5255762b73e8b195ca6d7302dda7833501ee8315
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-23 17:16:22 +0900

    ✨ feat: 모든 가게 조회

--- src/main/java/com/example/mokkoji/domain/store/service/StoreService.java
+++ src/main/java/com/example/mokkoji/domain/store/service/StoreService.java
@@ -1,6 +1,8 @@
 package com.example.mokkoji.domain.store.service;
 
+import com.example.mokkoji.domain.store.entity.Location;
 import com.example.mokkoji.domain.store.entity.Store;
+import com.example.mokkoji.domain.store.repository.LocationRepository;
 import com.example.mokkoji.domain.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -16,11 +18,18 @@ import java.util.List;
 public class StoreService {
 
     private final StoreRepository storeRepository;
+    private final LocationRepository locationRepository;
+
+    // 위치정보 가져오기
+
+    public List<Location> getAllLocations() {
+        return locationRepository.findAll();
+    }
 
     // 모든 가게 목록 조회
-//    public List<Store> findStoresWithin2Km(Double lat, Double lng) {
-//        return storeRepository.findAll(lat, lng);
-//    }
+     public List<Store> findStoresList() {
+            return storeRepository.findAll();
+        }
 
     // 특정 가게 조회
     public Store getStore(Long storeId) {
@@ -29,7 +38,7 @@ public class StoreService {
     }
 
     // 키워드 기반 가게 검색
-    public Page<Store> searchStores(String keyword, Pageable pageable) {
+    public List<Store> searchStores(String keyword, Pageable pageable) {
 //        storeRepository.findByNameContainingIgnoreCase(keyword, pageable);
         return null;
     }
